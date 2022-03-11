109ee59c4f13b03af1325aea5abd869785226438
Robin Dumont-Chaponet <src@dumontchapo.net>
2022-03-11 14:06:54 +0100

    Moved joins

--- src/Composed.php
+++ src/Composed.php
@@ -43,6 +43,9 @@ class Composed extends Simple
 		$this->queryString.= $this->getColumnsString();
 		$this->queryString.= !empty($this->commandEnd) ? $this->commandEnd.' ' : '';
 		$this->queryString.= $this->getFromString();
+
+		$this->queryString.= $this->getJoinString();
+
 		$this->queryString.= $this->getWhereString();
 		$this->queryString.= $this->getOrderString();
 		$this->queryString.= $this->getLimitOffsetString();
@@ -251,6 +254,48 @@ class Composed extends Simple
 			$this->where(...$where);
 
 		return $this;
+	}
+
+	// joins
+	public function join(Join $joinType, string $rightTableName, string $columnName, Comparator $comparator = Comparator::EQUAL, ?string $leftTableName = null, ?string $leftColumnName = null): static
+	{
+		$this->joins[] = [
+			'type' => $joinType,
+			'leftTableName' => $leftTableName,
+			'leftColumnName' => $leftColumnName ?? $columnName,
+			'rightTableName' => $rightTableName,
+			'rightColumnName' => $columnName,
+			'comparator' => $comparator,
+			'condition' => null, // WHERE B.key IS NULL
+		];
+
+		return $this;
+	}
+	public function getJoinString(): string
+	{
+		if(empty($this->joins))
+			return '';
+
+		$str = ' ';
+
+		// LEFT JOIN B ON A.key = B.key
+		foreach($this->joins as $join) {
+			$leftTableName = $join['leftTableName'];
+
+			if(empty($leftTableName) && count($this->tables) == 1) {
+				if(is_string($key = array_keys($this->tables)[0]))
+					$leftTableName = $key;
+				else
+					$leftTableName = static::quote(array_values($this->tables)[0]);
+			}
+
+			if(empty($leftTableName))
+				throw new \TypeError('Cannot join without left table name');
+
+			$str .= $join['type']?->value . ' ' . static::quote($join['rightTableName']) . ' ON ' . static::quote($leftTableName . '.' . $join['leftColumnName']) . ' ' . $join['comparator']->value . ' ' . static::quote($join['rightTableName'] . '.' . $join['rightColumnName']).' ';
+		}
+
+		return $str;
 	}
 
 	// order by
